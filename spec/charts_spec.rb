c1513c6e16bad4c0fd7e732f273065319f943035
Jelle Vandebeeck <jelle@fousa.be>
2011-04-03 21:41:03 +0200

    Shorten the rake

--- spec/charts_spec.rb
+++ spec/charts_spec.rb
@@ -3,8 +3,7 @@ require File.dirname(__FILE__) + '/spec_helper'
 describe 'The charts' do
   include Rack::Test::Methods
 
-	#%w(benelux alps westernswissalps).each do |country|
-	%w(germany).each do |country|
+	%w(germany benelux alps westernswissalps).each do |country|
 		it "should validate the links for #{country}" do
 			validate_links_for country
 		end
@@ -14,15 +13,16 @@ end
 def validate_links_for(country)
 	rasp = Rasp.new country
 	charts = rasp.charts
-	puts "======> #{country}"
+	puts "\n======> #{country}"
 	charts.keys.each do |header|
 		unless header == "config"
 			charts[header].keys.each do |chart|
 				puts "------> #{chart}"
 				urls = charts[header][chart]["today"]
-				urls.each do |url|
-					puts "---------> #{url}"
-					Net::HTTP.get_response(URI.parse(url)).class.should == Net::HTTPOK
+
+				if urls.first
+					puts "---------> #{urls.first}"
+					Net::HTTP.get_response(URI.parse(urls.first)).class.should == Net::HTTPOK
 				end
 			end
 		end
