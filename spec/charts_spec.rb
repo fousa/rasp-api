994dd04eaa90f0af0ad34006f8001eecb778a665
Jelle Vandebeeck <jelle@fousa.be>
2011-04-03 20:39:11 +0200

    Add alps charts

--- spec/charts_spec.rb
+++ spec/charts_spec.rb
@@ -3,24 +3,27 @@ require File.dirname(__FILE__) + '/spec_helper'
 describe 'The charts' do
   include Rack::Test::Methods
 
-  it "should have valid links" do
-		["benelux", "westernswissalps"].each do |country|
-			rasp = Rasp.new country
-			charts = rasp.charts
-			puts "=== COUNTRY: #{country}"
-			charts.keys.each do |header|
-				puts "====== HEADER: #{header}"
-				unless header == "config"
-					charts[header].keys.each do |chart|
-						puts "========= CHART: #{chart}"
-						urls = charts[header][chart]["today"]
-						urls.each do |url|
-							puts "=============== URL: #{url}"
-							Net::HTTP.get_response(URI.parse(url)).class.should == Net::HTTPOK
-						end
-					end
+	%w(benelux alps westernswissalps).each do |country|
+		it "should validate the links for #{country}" do
+			validate_links_for country
+		end
+	end
+end
+
+def validate_links_for(country)
+	rasp = Rasp.new country
+	charts = rasp.charts
+	puts "======> #{country}"
+	charts.keys.each do |header|
+		unless header == "config"
+			charts[header].keys.each do |chart|
+				puts "------> #{chart}"
+				urls = charts[header][chart]["today"]
+				urls.each do |url|
+					puts "---------> #{url}"
+					Net::HTTP.get_response(URI.parse(url)).class.should == Net::HTTPOK
 				end
 			end
 		end
-  end
+	end
 end
